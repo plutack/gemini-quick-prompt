42e33536c70e42654c2557851cb091ccb15d3af1
Talut Salako <salakotalut@gmail.com>
2024-11-11 10:02:07 +0100

    use os args to get user prompt

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 // configure the generative ai system first
-const systemPrompt string = "You are a software Developer assistant intended to gain insights about programming concepts particularly but not limited to functions in various programming languages and library. Your response is meant to be rendered on a terminal so markdown styled response is not needed. In cases where a manpage styled response is appropiate, generate response as such."
+const systemInstructionPrompt string = "You are a software Developer assistant intended to gain insights about programming concepts particularly but not limited to functions in various programming languages and library. Your response is meant to be rendered on a terminal so markdown styled response is not needed. In cases where a manpage styled response is appropiate, generate response as such."
 
 func printResponse(resp *genai.GenerateContentResponse) {
 	for _, cand := range resp.Candidates {
@@ -24,17 +24,19 @@ func printResponse(resp *genai.GenerateContentResponse) {
 }
 
 func main() {
+	if len(os.Args) != 2 {
+		panic("user prompt must be wrapped as a single string")
+	}
 	ctx := context.Background()
-	// get directly from enviromental variables
 	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("gemini_api_key")))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
 	model := client.GenerativeModel("gemini-1.5-flash") // is gemini flash the best model in this case?
-	model.SystemInstruction = genai.NewUserContent(genai.Text(systemPrompt))
+	model.SystemInstruction = genai.NewUserContent(genai.Text(systemInstructionPrompt))
 	// change to os args
-	resp, err := model.GenerateContent(ctx, genai.Text("chrome runtimeOnmessage with runtime sendmesssage"))
+	resp, err := model.GenerateContent(ctx, genai.Text(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
 	}

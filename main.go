1415fc3e4efa2e7eee3d969a0805cc130969db63
Talut Salako <salakotalut@gmail.com>
2025-05-06 08:15:30 +0100

    update to gemini flas h 2.5

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 // configure the generative ai system first
 const systemInstructionPrompt string = "You are a software Developer assistant intended to gain insights about programming concepts particularly but not limited to functions in various programming languages and library. Your response is meant to be rendered on a terminal and piped into a CLI that renders maarkdown(markdown styled response are greatly appreciated as they will be properly parsed). In cases where a manpage styled response is appropiate, generate response as such."
+const modelName = "gemini-2.5-flash-preview-04-17"
 
 func printResponse(resp *genai.GenerateContentResponse) {
 	for _, cand := range resp.Candidates {
@@ -33,7 +34,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer client.Close()
-	model := client.GenerativeModel("gemini-2.0-flash-exp") // changed from gemini-1.5-flash
+	model := client.GenerativeModel(modelName)
 	model.SystemInstruction = genai.NewUserContent(genai.Text(systemInstructionPrompt))
 	// change to os args
 	resp, err := model.GenerateContent(ctx, genai.Text(os.Args[1]))
